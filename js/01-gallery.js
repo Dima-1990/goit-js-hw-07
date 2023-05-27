d0c2caf71f1428d4193885be58cf986a5e446a02
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-05-27 19:41:05 +0300

    task 1 done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+//  console.log(galleryItems);
+
+const myGalleryUlEl = document.querySelector(".gallery");
+
+const galleryMasses = galleryItems.map(
+    ({ original, preview, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`).join("");
+
+myGalleryUlEl.innerHTML = galleryMasses;
+myGalleryUlEl.addEventListener("click", onClick);
+ 
+
+function onClick(e) {
+  e.preventDefault();
+
+    const instance = basicLightbox.create(`<img
+      src="${e.target.dataset.source}" width="100%" height="100%">`);
+    instance.show();
+
+
+    let statusInstance = instance.visible();
+
+    if (statusInstance) {
+        document.addEventListener("keydown", ({code})=> {
+              if (code !== "Escape") {
+                  return
+              }
+            
+              else {
+                instance.close();
+              }
+        });
+    }
+
+}
+
