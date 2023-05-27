6329ce8bac1c01388bc99909358169bf05300e58
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-05-27 21:42:26 +0300

    task 2 done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+
+const myGalleryUlEl = document.querySelector(".gallery");
+
+const galleryMasses = galleryItems
+  .map(
+    ({ original, preview, description }) => `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}"
+                alt="${description}" />
+      </a>
+    </li>`
+  )
+  .join("");
+
+  
+myGalleryUlEl.innerHTML = galleryMasses;
+  
+myGalleryUlEl.addEventListener("click", onClick);
+
+
+function onClick(e) {
+    e.preventDefault();
+
+   const lightbox = new SimpleLightbox(".gallery a", {
+     sourceAttr: "href",
+     captionDelay: 250,
+     captionSelector: "img",
+     captionsData: "alt",
+   });
+
+  lightbox.open();
+ }
+
+
