d988e00a947b29d02943a7cdf09ea29acfe9b627
Dima-1990 <99199420+Dima-1990@users.noreply.github.com>
2023-05-28 16:22:36 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,47 +1,46 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-//  console.log(galleryItems);
 
 const myGalleryUlEl = document.querySelector(".gallery");
 
-const galleryMasses = galleryItems.map(
+const galleryMarkup = galleryItems
+  .map(
     ({ original, preview, description }) => `<li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`).join("");
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </li>`
+  )
+  .join("");
 
-myGalleryUlEl.innerHTML = galleryMasses;
+myGalleryUlEl.innerHTML = galleryMarkup;
 myGalleryUlEl.addEventListener("click", onClick);
- 
 
 function onClick(e) {
   e.preventDefault();
 
-    const instance = basicLightbox.create(`<img
-      src="${e.target.dataset.source}" width="100%" height="100%">`);
-    instance.show();
+  if (!e.target.classList.contains("gallery__image")) {
+    return;
+  }
 
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="100%" height="100%">`
+  );
+  instance.show();
 
-    let statusInstance = instance.visible();
-
-    if (statusInstance) {
-        document.addEventListener("keydown", ({code})=> {
-              if (code !== "Escape") {
-                  return
-              }
-            
-              else {
-                instance.close();
-              }
-        });
+  const closeHandler = ({ code }) => {
+    if (code !== "Escape") {
+      return;
     }
 
-}
+    instance.close();
+    document.removeEventListener("keydown", closeHandler);
+  };
 
+  document.addEventListener("keydown", closeHandler);
+}
